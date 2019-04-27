c00eac60e79a4459b48be9011a16da3615180896
Tim Gossett <t-gossett@sbcglobal.net>
2019-04-27 15:42:07 -0500

    Add files via upload

---
+++ YOLO/ImageStartupScript.py
@@ -0,0 +1,6 @@
+cd Desktop
+cd Python
+cd ComputerVision
+cd YOLO
+
+python yolo.py -i  "C://Users//603516//Desktop//Python//ComputerVision//YOLO//images//dining_table.jpg" -y "C://Users//603516//Desktop//Python//ComputerVision//YOLO//yolo-coco" -c .5 -t .5
---
+++ YOLO/VideoStartupScript.py
@@ -0,0 +1,9 @@
+cd Desktop
+cd Python
+cd ComputerVision
+cd YOLO
+
+python yolo_video.py -i "C://Users//603516//Desktop//Python//ComputerVision//YOLO//videos//EllieMaddieRun.mp4" -o "C://Users//603516//Desktop//Python//ComputerVision//YOLO//output//EllieMaddieRun.mp4" -y "C://Users//603516//Desktop//Python//ComputerVision//YOLO//yolo-coco" -c .5 -t .5
+
+
+
